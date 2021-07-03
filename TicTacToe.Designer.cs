5183133dd8f15602dd486e4accaa85e0d66f71f8
Adrian Yabes <aidyabes13@gmail.com>
2021-07-03 11:41:12 +0800

    minimal changes

--- TicTacToe.Designer.cs
+++ TicTacToe.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Tic_Tac_Toe
 {
-    partial class Form1
+    partial class TicTacToe
     {
         /// <summary>
         /// Required designer variable.
@@ -29,7 +29,7 @@ namespace Tic_Tac_Toe
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TicTacToe));
             this.Uno = new System.Windows.Forms.Button();
             this.Dos = new System.Windows.Forms.Button();
             this.Tres = new System.Windows.Forms.Button();
